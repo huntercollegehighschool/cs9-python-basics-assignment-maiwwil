0526b9ec0532c8e222a24e52954ac81d8f9b8985
Maia Wilson <104437032+maiwwil@users.noreply.github.com>
2022-04-30 21:16:36 +0000

    I added the inputs for the other three numbers and just added them to the final print command

--- part5.py
+++ part5.py
@@ -11,5 +11,9 @@ What is your last name? Ketchum
 Hello, Ash Ketchum
 
 '''
+string1 = input("What's your first name?: ")
+string2 = input("And what's your last name?: ")
+
+print(" Hello,", string1, string2)
 
 #start writing your code below
